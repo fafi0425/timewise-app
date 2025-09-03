a55c8cbefceb27d57e78c9d91d96cb873e3c5065
Joven Dayanan <jovendayananterra123@gmail.com>
2025-09-03 23:49:03 +0000

    the error is still there, would you like me to provide the SDK code from

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,14 +3,15 @@ import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
+// Replace this with the configuration object from your Firebase project settings.
 const firebaseConfig = {
-  projectId: "timewise-8ivr2",
-  appId: "1:387642298145:web:581614b208c8a1c6f77860",
-  storageBucket: "timewise-8ivr2.firebasestorage.app",
   apiKey: "AIzaSyC6bkbCGIsNOTOmdtbSseOUbjue7bSEffw",
   authDomain: "timewise-8ivr2.firebaseapp.com",
-  measurementId: "",
-  messagingSenderId: "387642298145"
+  projectId: "timewise-8ivr2",
+  storageBucket: "timewise-8ivr2.appspot.com",
+  messagingSenderId: "387642298145",
+  appId: "1:387642298145:web:581614b208c8a1c6f77860",
+  measurementId: "G-XXXXXXXXXX" // Optional: Replace if you use Google Analytics
 };
 
 const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
