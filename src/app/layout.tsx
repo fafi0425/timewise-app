bffa8cfe4652529a995665051e8cd8f5cf758834
Joven Dayanan <jovendayananterra123@gmail.com>
2025-09-03 01:25:58 +0000

    can this app be installed on phone?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,6 +17,7 @@ const sourceCodePro = Source_Code_Pro({
 export const metadata: Metadata = {
   title: 'TimeWise',
   description: 'Employee Management System',
+  manifest: '/manifest.json',
 };
 
 export default function RootLayout({
@@ -30,6 +31,14 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600&family=Source+Code+Pro&family=Space+Grotesk:wght@400;700&display=swap" rel="stylesheet" />
+        <meta name="application-name" content="TimeWise" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content="TimeWise" />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="theme-color" content="#0F4C75" />
+        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
       </head>
       <body
         className={`${inter.variable} ${spaceGrotesk.variable} ${sourceCodePro.variable} font-body antialiased`}
